128a2deba1b7b2e6c9192fa448e5c59f5ff56b2f
Lior <levydlior@gmail.com>
2022-07-29 12:23:25 -0600

    started to build tiles

--- client/src/Components/MyCities.js
+++ client/src/Components/MyCities.js
@@ -12,18 +12,12 @@ function MyCities() {
     });
   }, []);
 
+  
   const tilesList = myCities.map((city) => {
-    return <Tile cityName={city.name} cityId={city.id} />;
+    return <Tile city={city} />;
   });
 
-  // useEffect(() => {
-  //   fetch('/https://api.openweathermap.org/data/2.5/weather?q=San%20Diego&APPID=e29814a14c69b8a0274291687bb9ae09')
-  //     .then(r => {
-  //       if(r.ok){
-  //         r.json(). then (cities => console.log(cities))
-  //       }
-  //     })
-  // },[])
+
 
   return <div id="my-cities-page">{tilesList}</div>;
 }
--- client/src/Components/Tile.js
+++ client/src/Components/Tile.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Tile() {
-  return <div className="tile">Tile</div>;
+function Tile({ city }) {
+  const [fetchedCity, setFetchedCity] = useState({});
+  const [loaded, setLoaded] = useState(false)
+  const cityName = city.name;
+
+  useEffect(() => {
+    if (!loaded){
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=940fcac699ff529fe9e0a4a0de433a1c`
+    )
+      .then((r) => r.json())
+      .then((weather) => {
+        setFetchedCity(weather)
+        setLoaded(true)
+    })}
+  }, []);
+
+  console.log(fetchedCity);
+
+  return (
+    <div className="tile">
+      <h2>{cityName}</h2>
+      <p>Current temperature: {fetchedCity.main.temp} </p>
+    </div>
+  );
 }
+
+
+
 
 export default Tile;
