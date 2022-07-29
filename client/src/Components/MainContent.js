acf780264fdef7ea2cd7e9cefc0770e6dfe4b4dc
Shane Dorsey <scdorsey22@gmail.com>
2022-07-29 16:09:30 -0700

    Styling

--- client/src/Components/MainContent.js
+++ client/src/Components/MainContent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import SearchComponent from "./SearchComponent";
 import WeatherComponent from "./WeatherComponent";
 
