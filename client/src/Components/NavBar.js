ba16e72ff7f579d9fc5495df5adc995495d3fca3
Lior <levydlior@gmail.com>
2022-07-29 10:05:29 -0600

    add headet and nav component, add some style

---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+
+function NavBar({loggedUser, onLogOut}) {
+  return (
+    <nav id="header-nav">
+      {!loggedUser 
+       ? null
+       : (
+        <>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink onClick={onLogOut} to="/">
+            Logout
+          </NavLink>
+        </>
+      )}
+    </nav>
+  );
+}
+
+export default NavBar;
