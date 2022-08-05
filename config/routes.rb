eb12d22b89fc801df7a48ac6a913062a2201fa1b
Shane Dorsey <scdorsey22@gmail.com>
2022-08-05 08:33:23 -0700

    fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
